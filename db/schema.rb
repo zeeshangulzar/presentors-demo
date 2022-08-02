ddbcd13ade1c37833069aedab5055e20a7dff27e
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-08-02 15:48:44 +0500

    Updated user present and add birth year method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_080652) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_103324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,10 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_080652) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "birthdate"
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "birthdate"
   end
 
 end
